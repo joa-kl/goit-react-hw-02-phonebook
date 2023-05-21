a4cf2ee465d798558bd797a81cc62bba6e5e20aa
Joanna K <joannakleinrm@gmail.com>
2023-05-21 15:02:33 +0200

    jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Form } from "./Form/Form";
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+}
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Form />
+      </div>
+    );
+  }
 };
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,9 @@
+const { Component } = require("react");
+
+class Contacts extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
